3dfaffaa49aa699eb7a55c017428ff08a4bcc718
Andreick <andreick.gomes.ismart@gmail.com>
2023-02-26 17:40:39 -0300

    + Implementa tela de login e requisição para o back

---
+++ src/app/core/services/login.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { CredenciaisJogador } from '../models/credenciais-jogador.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private readonly API = `${environment.urlBackend}/jogador/login`
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  authenticate(credenciais: CredenciaisJogador): Observable<void> {
+    return this.http.post<void>(this.API, credenciais)
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { HeaderComponent } from './components/header/header.component';
+import { InputValidationComponent } from './components/input-validation/input-validation.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, InputValidationComponent],
   imports: [
     CommonModule,
     CardModule,
     ButtonModule,
-    MenubarModule
+    MenubarModule,
   ],
   exports: [
     CommonModule,
+    ReactiveFormsModule,
     CardModule,
     ButtonModule,
     MenubarModule,
-  ]
+    HeaderComponent,
+    InputValidationComponent
+  ],
 })
 export class SharedModule { }
