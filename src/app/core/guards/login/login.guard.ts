9a73dd74c15ecb48bbbb84ff67946b0b4e6a6678
Andreick <andreick.gomes.ismart@gmail.com>
2023-03-04 21:25:56 -0300

    + Cria guardas de rotas que redirecionam o jogador

---
+++ src/app/core/guards/login/login.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Route, UrlSegment, UrlTree, Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { JogadorLoginService } from '../../services/jogador/jogador-login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate, CanLoad {
+
+  constructor(
+    private jogadorLoginService: JogadorLoginService,
+    private router: Router
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.checkLogin()
+  }
+
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkLogin()
+  }
+
+  private checkLogin(): true | UrlTree {
+    if (this.jogadorLoginService.isLoggedIn()) {
+      return this.router.parseUrl('stefamons')
+    }
+    return true;
+  }
+}
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginGuard } from 'src/app/core/guards/login/login.guard';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent,
+    path: '', component: HomeComponent, canActivate: [LoginGuard],
     children: [
-      { path: '', component: LoginComponent },
-      { path: 'signup', component: SignupComponent }
+      { path: '', component: LoginComponent, canActivate: [LoginGuard] },
+      { path: 'signup', component: SignupComponent, canActivate: [LoginGuard] }
     ]
   }
 ];
